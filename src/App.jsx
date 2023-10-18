567287d9705834eef9dc9c62b616495f9db4605b
하우그로우 <howgrow@haugeulouui-MacBookAir.local>
2023-10-18 21:47:23 +0900

    a little exercise done. I am so tired today. 18/10

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,32 @@
 import { useState } from "react";
-import Button from "./Button";
-
-const INITIAL_VALUE = ["A", "B", "C"];
+import "./app.scss";
 const App = () => {
-  const [arrList, setArrList] = useState(INITIAL_VALUE);
-  const [inputVal, setInputVal] = useState("");
-  const removeTheFirstLetter = () => {
-    setArrList((cur) => cur.slice(1));
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const handleAdd = () => {
+    setAge((cur) => cur + 1);
   };
 
-  const removeSpecificLetter = (letter) => {
-    setArrList((cur) => cur.filter((el) => el !== letter));
-  };
-
-  const addToTheStart = (letter) => {
-    setArrList((cur) => [letter, ...cur]);
-  };
-
-  const addToTheEnd = (letter) => {
-    setArrList((cur) => [...cur, letter]);
-  };
-
-  const handleClear = () => {
-    setArrList([]);
-  };
-
-  const handleReset = () => {
-    setArrList(INITIAL_VALUE);
-  };
-
-  const handleUpdateAtoH = () => {
-    setArrList((cur) => cur.map((el) => (el === "A" ? "H" : el)));
-  };
-
-  const handleChange = (e) => {
-    setInputVal(e.target.value);
-  };
-
-  const addInputToTheStart = () => {
-    setArrList((cur) => [inputVal, ...cur]);
-  };
-
-  const addAtAnyIndex = (el, index) => {
-    setArrList((cur) => [...cur.slice(0, index), el, ...cur.slice(index)]);
-    // numbers.splice(2, 0, 3);
+  const handleSubtract = () => {
+    if (age <= 0) {
+      alert("cannot go lower than 0");
+    } else {
+      setAge((cur) => cur - 1);
+    }
   };
   return (
-    <div>
-      <Button name="Remove the first letter" onClick={removeTheFirstLetter} />
-      <Button
-        name="Remove specific letter"
-        onClick={() => removeSpecificLetter("A")}
+    <div className="container">
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
-      <Button
-        name="Add a new element to the start"
-        onClick={() => addToTheStart("K")}
-      />
-      <Button
-        name="Add a new element to the end"
-        onClick={() => addToTheEnd("Q")}
-      />
-
-      <Button name="Clear" onClick={handleClear} />
-      <Button name="Reset" onClick={handleReset} />
-      <Button name="Update A to H" onClick={handleUpdateAtoH} />
-      <Button name="Add to the first" onClick={addInputToTheStart} />
-      <Button
-        name="add a new element at any index"
-        onClick={() => addAtAnyIndex(inputVal, 2)}
-      />
-
-      <input type="text" value={inputVal} onChange={handleChange} />
-      {arrList.join(", ")}
+      <div className="btn-container">
+        <button onClick={handleAdd}>+</button>
+        {age}
+        <button onClick={handleSubtract}>-</button>
+      </div>
+      My name is {name} and I am {age} years old.
     </div>
   );
 };
