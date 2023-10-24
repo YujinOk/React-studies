b53ae73d829f775fc2919e11146afbf1861e1d9a
하우그로우 <howgrow@haugeulouui-MacBookAir.local>
2023-10-24 21:26:20 +0900

    had a good practice with useEffect and rendering the list of data

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [items, setItems] = useState([
-    { id: crypto.randomUUID(), name: "Item 1" },
-    { id: crypto.randomUUID(), name: "Item 2" },
-  ]);
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    const controller = new AbortController();
+    async function logUsers() {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users",
+          {
+            signal: controller.signal,
+          }
+        );
 
-  const addItem = () => {
-    setItems((curItems) => {
-      return [...curItems, { id: crypto.randomUUID(), name: "New Item" }];
-    });
-  };
+        const users = await response.json();
+        setUsers(users);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    logUsers();
+    return () => {
+      controller.abort();
+    };
+  }, []);
 
   return (
     <div>
-      <button onClick={addItem}>Add Item</button>
-      <pre>
-        {items.map((item) => {
-          return (
-            <React.Fragment key={item.id}>
-              {item.name}
-              <input type="text" />
-            </React.Fragment>
-          );
-        })}
-      </pre>
+      <h1>User List</h1>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <ul>
+          {users?.map((curUser) => (
+            <li key={curUser.id}>{curUser.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
