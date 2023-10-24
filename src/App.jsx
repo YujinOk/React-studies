2ad24fb70098bc5216e966e6039a922732a30a09
하우그로우 <howgrow@haugeulouui-MacBookAir.local>
2023-10-24 19:36:27 +0900

    when rendering list in fragmenent, you need key and it needs React.Fragment instead of empty fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,30 @@
-import { useState, useEffect } from "react";
-import "./app.scss";
-import Child from "./Child";
+import React, { useState } from "react";
+
 const App = () => {
-  const [users, setUsers] = useState();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
-  // https://jsonplaceholder.typicode.com/users
+  const [items, setItems] = useState([
+    { id: crypto.randomUUID(), name: "Item 1" },
+    { id: crypto.randomUUID(), name: "Item 2" },
+  ]);
 
-  useEffect(() => {
-    setLoading(true);
-    setError(undefined);
-    const controller = new AbortController();
-
-    fetch("https://jsonplaceholder.typicode.com/users", {
-      signal: controller.signal,
-    })
-      .then((res) => {
-        if (res.status === 200) {
-          return res.json();
-        } else {
-          return Promise.reject(res);
-        }
-      })
-      .then((data) => {
-        console.log("HERE");
-        setUsers(data);
-      })
-      .catch((e) => {
-        if (e?.name === "AbortError") return;
-        setError(e);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-
-    return () => {
-      controller.abort();
-    };
-  }, []);
+  const addItem = () => {
+    setItems((curItems) => {
+      return [...curItems, { id: crypto.randomUUID(), name: "New Item" }];
+    });
+  };
 
   return (
-    <div className="container">
-      <h1>Users</h1>
-      {loading ? (
-        <h2>Loading...</h2>
-      ) : error ? (
-        <h2>Error!</h2>
-      ) : (
-        <pre>{JSON.stringify(users, null, 2)}</pre>
-        // <p>{JSON.stringify(users)}</p>
-      )}
+    <div>
+      <button onClick={addItem}>Add Item</button>
+      <pre>
+        {items.map((item) => {
+          return (
+            <React.Fragment key={item.id}>
+              {item.name}
+              <input type="text" />
+            </React.Fragment>
+          );
+        })}
+      </pre>
     </div>
   );
 };
--- src/Child.jsx
+++ src/Child.jsx
@@ -1,50 +1,24 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
-const Child = () => {
+const Child = ({ favoriteNumber }) => {
   const [name, setName] = useState("");
   const [age, setAge] = useState(0);
-  const [inputVal, setInputVal] = useState("");
 
-  useEffect(() => {
-    console.log("Re-Render");
-  });
-
-  useEffect(() => {
-    console.log("Hi");
-    return () => {
-      console.log("Bye");
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log(`My name is ${name} and ${age} years old`);
-  }, [name, age]);
-
-  useEffect(() => {
-    document.title = name;
-    const timeId = setTimeout(() => {
-      console.log(`My name is ${name}`);
-    }, 1000);
-
-    return () => {
-      clearTimeout(timeId);
-    };
-  }, [name, age]);
-
-  const handleChange = (e) => {
-    setInputVal(e.target.value);
-    setName(e.target.value);
-  };
   return (
     <div>
-      <input type="text" value={inputVal} onChange={handleChange} />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <input
         type="number"
         value={age}
         onChange={(e) => setAge(e.target.value)}
       />
-      My name is {name} and {age} years old
+      {!!favoriteNumber && `My favorite number is ${favoriteNumber}`}
+      {/* My name is {name} and {age} years old */}
     </div>
   );
 };
