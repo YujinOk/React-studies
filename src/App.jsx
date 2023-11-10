0479406583953c4a2b8bc6b7054d33859818b090
YujinOk <yujinok63@gmail.com>
2023-11-10 19:07:33 +1100

    still in progess to do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,38 @@
 import React, { useEffect, useState } from "react";
+import "./app.scss";
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-  useEffect(() => {
-    const controller = new AbortController();
-    async function logUsers() {
-      try {
-        setLoading(true);
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users",
-          {
-            signal: controller.signal,
-          }
-        );
-
-        const users = await response.json();
-        setUsers(users);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-    logUsers();
-    return () => {
-      controller.abort();
-    };
-  }, []);
-
+  const [toDo, setTodo] = useState("");
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+  console.log(toDo);
   return (
-    <div>
-      <h1>User List</h1>
-      {isLoading ? (
-        <h2>Loading...</h2>
-      ) : (
-        <ul>
-          {users?.map((curUser) => (
-            <li key={curUser.id}>{curUser.name}</li>
-          ))}
+    <>
+      {toDo && (
+        <ul id="list">
+          <li class="list-item">
+            <label class="list-item-label">
+              <input type="checkbox" data-list-item-checkbox />
+              <span data-list-item-text>Item 1</span>
+            </label>
+            <button data-button-delete>Delete</button>
+          </li>
+          <li class="list-item">
+            <label class="list-item-label">
+              <input type="checkbox" checked data-list-item-checkbox />
+              <span data-list-item-text>Item 2</span>
+            </label>
+            <button data-button-delete>Delete</button>
+          </li>
         </ul>
       )}
-    </div>
+      <div id="new-todo-form">
+        <label htmlFor="todo-input">New Todo</label>
+        <input type="text" id="todo-input" onChange={handleChange} />
+        <button>Add Todo</button>
+      </div>
+    </>
   );
 };
 
